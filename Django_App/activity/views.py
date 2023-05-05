5ce910a27594bf214426c136aa900bb632a43b22
mappii130 <90373590+mappii130@users.noreply.github.com>
2023-05-05 18:17:43 +0900

    homeへdateとdurationをリストにして表示

--- Django_App/activity/views.py
+++ Django_App/activity/views.py
@@ -4,46 +4,23 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import ActivityRecord
 from .forms import ActivityRecordForm
-from . import models
-from . import graph
-import datetime
-from collections import defaultdict
 
 # ホーム画面(仮)
 # Todo: 未完了
 class HomeView(LoginRequiredMixin, generic.TemplateView):
     #テンプレートファイル連携
     template_name = 'home.html'
-    
-    #変数としてグラフイメージをテンプレートに渡す
+
     def get_context_data(self, **kwargs):
-        # ページを表示している日付を取得
-        today = datetime.date.today()
-        # ページを表示している日付から7日分の日付を取得
-        date_list = [today - datetime.timedelta(days=x) for x in range(6, -1, -1)]
-        date_list.reverse()
-        # グラフのデータを取得
-        qs = models.ActivityRecord.objects.filter(date__in=date_list)  
-        # 同じ日付のデータを合算するための辞書を用意
-        duration_dict = defaultdict(int)
-        for activity in qs:
-            duration_dict[activity.date] += activity.duration
-        # グラフに反映するためのデータを作成
-        x = []      #X軸データ
-        y = []      #Y軸データ
-        for date in date_list:
-            x.append(date)
-            y.append(duration_dict.get(date, 0)) #データが存在しない場合は0を代入
-        chart = graph.Plot_Graph(x,y) #グラフ作成
-        #変数を渡す
         context = super().get_context_data(**kwargs)
-        context['chart'] = chart
+        records = ActivityRecord.objects.all()
+        date_list = [record.date.strftime('%-m/%-d') for record in records]
+        duration_list = [record.duration for record in records]
+        duration_list = [round(record.duration / 60, 2) for record in records] # 分単位から小数点第２位の時間に変換
+        context['date_list'] = date_list
+        context['duration_list'] = duration_list
         return context
 
-    #get処理
-    def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
-    
 
 # 積み上げ追加画面
 class ActivityAddView(LoginRequiredMixin, generic.CreateView):
