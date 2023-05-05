025c36fae9fd8bddcaf75151275a8596eb156790
DanK04 <kawazudan0430@gmail.com>
2023-05-05 23:27:32 +0900

    add success modal

--- Django_App/activity/views.py
+++ Django_App/activity/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from .models import ActivityRecord
 from .forms import ActivityRecordForm
 
@@ -20,6 +24,35 @@ class ActivityAddView(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            return self.ajax_submit(request)
+        return super(ActivityAddView, self).dispatch(request, *args, **kwargs)
+
+    def ajax_submit(self, request):
+        form = self.form_class(request.POST, instance=ActivityRecord(user=request.user))
+
+        if form.is_valid():
+            form.save()
+            return JsonResponse({"success": True})
+        else:
+            return JsonResponse({"success": False, "errors": form.errors})
+
+# 累計日数の取得
+def get_total_days(request):
+    user = request.user
+    if not user.is_authenticated:
+        return JsonResponse({"error": "User not authenticated"}, status=401)
+
+    # 日付を一意にしたデータセットを取得
+    unique_dates = ActivityRecord.objects.filter(user=user).values('date').annotate(count=Count('date'))
+
+    # 一意な日付の数をカウント
+    total_days = len(unique_dates)
+
+    return JsonResponse({"total_days": total_days})
 
 # レコード画面(積み上げ一覧)
 class ActivityListView(LoginRequiredMixin, generic.ListView):
