e2b7e1edebe212fbe660f2beb3dbc64d1d27762a
mappii130 <90373590+mappii130@users.noreply.github.com>
2023-05-07 00:12:04 +0900

    home画面へデータをJSON形式で渡す

--- Django_App/activity/views.py
+++ Django_App/activity/views.py
@@ -6,28 +6,61 @@ from django.db.models import Count
 from django.http import JsonResponse
 from .models import ActivityRecord
 from .forms import ActivityRecordForm
+from datetime import timedelta, date
+from django.http import JsonResponse
 
 # ホーム画面
-class HomeView(LoginRequiredMixin, generic.TemplateView):
-    template_name = 'home.html'   #テンプレートファイル連携
+class HomeView(LoginRequiredMixin, generic.View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['username'] = self.request.user.user_name   # Usersからユーザー名前を取得
-        records = ActivityRecord.objects.all()   # ActivityRecordの全てのデータを取得する
-        date_list = [record.date.strftime('%-m/%-d') for record in records]  # 日付のリストを作成し、'%-m/%-d'のフォーマットに変換する
-        duration_list = [record.duration for record in records]   # 各レコードのdurationをduration_listに格納する
-        total_duration = sum(duration_list) // 60, sum(duration_list) % 60  # duration_listから総時間を計算する
-        total_duration_str = f"{total_duration[0]}時間{total_duration[1]}分" # 計算結果を'時間:分'の形式に変換してtotal_duration_strに格納する
-        total_days = len(set(date_list))   # date_listから総日数を計算する
-        duration_list = [round(record.duration / 60, 2) for record in records] # duration_listから各durationを'時間.分'の形式に変換してduration_listに格納する
+    def get(self, request, *args, **kwargs):
+        # 過去すべてのレコードを取得する
+        end_date = date.min
+        records = ActivityRecord.objects.filter(user=self.request.user, date__gte=end_date)
 
-        # コンテキストに必要な変数を格納し、returnする
-        context['date_list'] = date_list
-        context['duration_list'] = duration_list
-        context['total_duration'] = total_duration_str
-        context['total_days'] = total_days
-        return context
+        # 日付ごとのレコードのリストを取得する
+        records_by_date = {}
+        for record in records:
+            date_str = record.date.strftime('%-m/%-d')
+            if date_str not in records_by_date:
+                records_by_date[date_str] = []
+            records_by_date[date_str].append(record)
+
+        # 日付のリストを作成する
+        date_list = []
+        # 日付から7日前までの日付を取得する
+        current_date = date.today()
+        for i in range(7):
+            date_str = current_date.strftime('%-m/%-d')
+            date_list.append(date_str)
+            current_date -= timedelta(days=1)
+        date_list.reverse()
+
+        # 日付ごとのdurationを合計したリストを作成する
+        duration_list = []
+        for date_str in date_list:
+            if date_str in records_by_date:
+                duration_sum = sum([record.duration for record in records_by_date[date_str]])
+                duration_list.append(duration_sum)
+            else:
+                duration_list.append(0)
+
+        # 日付の重複を除いた日数を計算する
+        total_days = len(set(records_by_date.keys()))
+
+        # 全レコードのdurationを合計する
+        total_duration = sum([record.duration for record in records])
+
+        # duration_listから各durationを'時間.分'の形式に変換する
+        duration_list = [f"{duration // 60 + duration % 60 / 60:.1f}" for duration in duration_list]
+
+        # JSON形式でデータを返す
+        response_data = {
+            'date_list': date_list,
+            'duration_list': duration_list,
+            'total_days': total_days,
+            'total_duration': f"{total_duration // 60}:{total_duration % 60}"
+        }
+        return JsonResponse(response_data)
 
 
 # 積み上げ追加画面
