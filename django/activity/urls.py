e2901c52dda34bbb8e225f28c7a89d7c7f1c141f
DanK04 <kawazudan0430@gmail.com>
2023-05-11 00:34:11 +0900

    add ajax view in activity app

--- django/activity/urls.py
+++ django/activity/urls.py
@@ -5,7 +5,9 @@ app_name = 'activity'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('ajax_get_data/', views.HomeAjaxView.as_view(), name='home_ajax'),
     path('list/', views.ActivityListView.as_view(), name='activity_list'),
+    path('list/ajax_get_data/', views.ActivityListAjaxView.as_view(), name='list_ajax'),
     path('add/', views.ActivityAddView.as_view(), name='activity_add'),
     path('add/ajax_submit/', views.ActivityAddView.as_view(), name='activity_ajax_submit'),
     path('<int:pk>/edit/', views.ActivityEditView.as_view(), name='activity_edit'),
